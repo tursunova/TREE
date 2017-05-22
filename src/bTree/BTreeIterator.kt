ae6c730f735ad63fd99b398d7ce8cd03ffdd5521
Munira <tursunova.munira@gmail.com>
2017-05-22 13:04:31 +0200

    Update BTreeIterator.kt

--- src/bTree/BTreeIterator.kt
+++ src/bTree/BTreeIterator.kt
@@ -1,25 +1,30 @@
 package bTree
 
-class BTreeIterator<T : Comparable<T>, P>(node: BNode<T, P>?) : Iterator<BNode<T, P>> {
+class BTreeIterator<T : Comparable<T>, P>(var node: BNode<T, P>?) : Iterator<BNode<T, P>> {
 
-    var q = BQueue<T,P>()
+    var q = mutableListOf<BNode<T, P>?>()
 
     init {
-        q.push(node!!)
+        q.add(node)
     }
 
-    override fun hasNext(): Boolean = !q.empty()
+    override fun hasNext(): Boolean{
+        if (node == null)
+            return false
+        return q.isNotEmpty()
+    }
+
 
     override fun next(): BNode<T, P> {
-        val v = q.pop()
-        if (!v.data.isLeaf()){
+        val v = q[0]
+        if (!v!!.isLeaf()){
 
-            for(child in v.data.children){
-                q.push(child)
+            for(child in v.children){
+                q.add(child)
             }
 
         }
-        return v.data
+        return v
     }
 
 }
