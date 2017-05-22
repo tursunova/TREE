9d2581fda94b316123ade4f50dd896a98d43bc6d
Munira <tursunova.munira@gmail.com>
2017-05-22 12:32:04 +0200

    Update Node.kt

--- src/Node.kt
+++ src/Node.kt
@@ -7,6 +7,9 @@ class Node<T: Comparable<T>, P>(var key: T, var value: P, var color: Color = Col
     var rightChild: Node<T,P>? = null
     fun isRoot(): Boolean = (parent == null)
     fun isLeaf(): Boolean = ((leftChild == null) && (rightChild == null))
-
+    @Override
+    fun equals(newNode:Node<T,P>):Boolean{
+        return this.key == newNode.key && this.value == newNode.value && this.color == newNode.color
+    }
 }
 
